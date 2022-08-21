d1a5db3acecb1863d2bf4622313a78670708376d
Evgenii B <budich@me.com>
2022-08-22 00:33:32 +0300

    Added hw Task 002 Min&Max  Project

---
+++ hw_task002/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// int max = firstNumber;
+// int min = secondNumber;
+
+// if(secondNumber > firstNumber)
+// {
+//     max = secondNumber; min = firstNumber;
+// }
+// Console.WriteLine("Большее число: "+max + " Меньшее число: "+min);
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber) {
+    System.Console.WriteLine("Большее число: "+ firstNumber+ " Меньшее число: "+secondNumber);
+}
+else if (secondNumber > firstNumber) {
+    System.Console.WriteLine("Большее число: "+ secondNumber+ " Меньшее число: "+firstNumber);
+}
+else {
+    System.Console.WriteLine("Введённые числа равны");
+}
