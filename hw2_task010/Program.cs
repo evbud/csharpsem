f91ce9bc05a38890b007aa1ad6523bd5b3d6756f
Evgenii B <budich@me.com>
2022-08-23 11:51:51 +0300

    Added hw2 Task 010 The Second Digit Of A Three-digit Number Project

---
+++ hw2_task010/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter 3-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine($"The second digit in your number: {((number % 100) / 10)}");
+}
+else
+{
+    Console.WriteLine("Bad number");
+}
