1eedacc53341c68037cab885f074aecfc9739568
Evgenii B <budich@me.com>
2022-08-23 14:48:42 +0300

    Added hw2 Task 013 Third Digit Project

---
+++ hw2_task013/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Enter number: ");
+string number = Console.ReadLine();
+
+if (number.Length < 3)
+{
+    Console.WriteLine("There is no third digit");
+}
+else
+{
+    Console.WriteLine(number[2]);
+}
