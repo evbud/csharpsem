ceeed2c6f4a4b600204ef91221ceff80d15f2b0f
Evgenii B <budich@me.com>
2022-08-23 16:03:51 +0300

    Added hw2 Task 015 Weekend Check Project

---
+++ hw2_task015/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Enter number of the day of the week: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    switch(number)
+{
+    case 6 : 
+    case 7 : 
+        Console.WriteLine("Yes"); 
+    break;
+    default: 
+        Console.WriteLine("No"); 
+    break;
+}
+}
+else
+{
+    Console.WriteLine("Bad number");
+}
